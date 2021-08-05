e00426d4e018069902e308209a345179180c9b6b
ksju6561 <49939845+ksju6561@users.noreply.github.com>
2021-08-05 10:26:51 +0900

    Update MerkleTreePathGadget_MiMC7.java
    fix bug

--- JsnarkCircuitBuilder/src/examples/gadgets/hash/MerkleTreePathGadget_MiMC7.java
+++ JsnarkCircuitBuilder/src/examples/gadgets/hash/MerkleTreePathGadget_MiMC7.java
@@ -1,9 +1,20 @@
+/*******************************************************************************
+ * Author: Ahmed Kosba <akosba@cs.umd.edu>
+ *******************************************************************************/
 package examples.gadgets.hash;
 
 import circuit.operations.Gadget;
 import circuit.structure.Wire;
 
+/**
+ * A Merkle tree authentication gadget using the subsetsum hash function
+ * 
+ */
+
 public class MerkleTreePathGadget_MiMC7 extends Gadget {
+
+	//private static int digestWidth = SubsetSumHashGadget.DIMENSION; // 3
+
 	private int treeHeight;
 	private Wire directionSelectorWire;
 	private Wire[] directionSelectorBits;
@@ -24,22 +35,28 @@ public class MerkleTreePathGadget_MiMC7 extends Gadget {
 	}
 
 	private void buildCircuit() {
+		//directionSelectorBits = new viewWireGadget(new Wire[] {directionSelectorWire}, treeHeight).getOutputWires();
 		directionSelectorBits = directionSelectorWire.getBitWires(treeHeight).asArray();
 
-
+		// Apply CRH to leaf data
 		MiMC7Gadget MiMC7 = new MiMC7Gadget(leafWires);
         Wire currentHash = MiMC7.getOutputWires()[0];
 
-		Wire inHash, temp, temp2;
+		// Apply CRH across tree path guided by the direction bits
+		Wire temp, temp2;
+		Wire[] inHash = new Wire[2];
 		for (int i = 0; i < treeHeight; i++) {
-			temp = currentHash.sub(intermediateHashWires[i]);
-			temp2 = directionSelectorBits[i].mul(temp);
-			inHash = intermediateHashWires[i].add(temp2);
-	
-			temp = currentHash.add(intermediateHashWires[i]);
-			inHash = temp.sub(inHash);
-			
-			MiMC7 = new MiMC7Gadget(currentHash, inHash);
+            temp = currentHash.sub(intermediateHashWires[i]);
+            temp2 = directionSelectorBits[i].mul(temp);
+            inHash[0] = intermediateHashWires[i].add(temp2);
+            temp = currentHash.add(intermediateHashWires[i]);
+            inHash[1] = temp.sub(inHash[0]);
+
+			// in0 = (c-inter)*d + inter
+			// in1 = (c+inter) - in0			
+			// d:0 > inter / c
+			// d:1 > c / inter            
+            MiMC7 = new MiMC7Gadget(inHash);
             currentHash = MiMC7.getOutputWires()[0];
 		}
 		outRoot = new Wire[] {currentHash};
