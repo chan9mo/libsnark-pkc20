900c3607104f867a266f442e84776645b54ddaaf
ksju6561 <49939845+ksju6561@users.noreply.github.com>
2021-08-06 13:38:19 +0900

    Update Tally.java
    fix ec pairing bug
    Modified to generate multiple voter messages.
    default is to create a message of 1000 voters, and the number of voters can be set as an argument. ex) examples.generators.Tally 10000
    In the real tally protocols, EC_V, EC_W must be decrypted through java_test/DecryptMessage.java to find out the msg, and then the calculation must be performed.

--- JsnarkCircuitBuilder/src/examples/generators/Tally.java
+++ JsnarkCircuitBuilder/src/examples/generators/Tally.java
@@ -6,39 +6,32 @@ package examples.generators;
 import java.math.BigInteger;
 // import java.math.*;
 
-import util.Util;
-import circuit.config.Config;
 import circuit.eval.CircuitEvaluator;
 import circuit.structure.CircuitGenerator;
-import circuit.structure.ConstantWire;
 import circuit.structure.Wire;
-import circuit.structure.WireArray;
-import examples.gadgets.hash.MerkleTreePathGadget;
-import examples.gadgets.hash.SHA256Gadget;
-import examples.gadgets.hash.SubsetSumHashGadget;
 import examples.gadgets.diffieHellmanKeyExchange.ECGroupGeneratorGadget;
 import examples.gadgets.diffieHellmanKeyExchange.ECGroupOperationGadget;
-import examples.gadgets.math.ModConstantGadget;
 
+import java_test.GroupElement;
+import java_test.Curve25519;
+import java.security.SecureRandom;
 
 public class Tally extends CircuitGenerator {
     /* INPUT */
-    private Wire G;
-    private Wire U;
-    private Wire V;
-    private Wire W;
+    private Wire Gx, Gy;
+    private Wire Ux, Uy;
+    private Wire Vsum_x, Vsum_y;
+    private Wire Wsum_x, Wsum_y;
     private Wire msgsum; //MAX = (15) * 2 ^ 16
     /* WITNESS */
     private Wire SK;
 
     private int numofelector;
-    private int treeHeight;
     public static final int EXPONENT_BITWIDTH = 254; // in bits
 
 
-    public Tally(String circuitName, int treeHeight, int numofelector) {
+    public Tally(String circuitName, int numofelector) {
         super(circuitName);
-        this.treeHeight = treeHeight;
         this.numofelector = numofelector;
     }
 
@@ -47,56 +40,105 @@ public class Tally extends CircuitGenerator {
 		return output;
     }
     
-    private Wire mulexp(Wire a, Wire b){
-		ModConstantGadget mod = new ModConstantGadget(a, b, Config.CURVE_ORDER);
-		return mod.getOutputWires()[0]; 
-	}
-
-    
     @Override
     protected void buildCircuit(){
-        G = createInputWire("G");
-        U = createInputWire("U");
-        V = createInputWire("V"); //vsum
-        W = createInputWire("W"); //wsum
-      
+        Gx = createInputWire("Gx");         Gy = createInputWire("Gy");
+        Ux = createInputWire("Ux");         Uy = createInputWire("Uy");
+        Vsum_x = createInputWire("Vsum_x"); Vsum_y = createInputWire("Vsum_y"); /*vsum*/
+        Wsum_x = createInputWire("Wsum_x"); Wsum_y = createInputWire("Wsum_y"); /*wsum*/
         msgsum = createInputWire("msgsum");
-		Wire rand = createConstantWire(new BigInteger("123141251243"));
 
         SK = createProverWitnessWire("sk");
-        ECGroupGeneratorGadget dec1 = new ECGroupGeneratorGadget(G, SK);
-        Wire check1 = dec1.getOutputPublicValue();
-        // makeOutput(check1, "dec1");
-        addEqualityAssertion(check1, U, "check1");
 
-        ECGroupOperationGadget dec = new ECGroupOperationGadget(V, SK, G, mulexp(rand, msgsum));
-        Wire check2 = dec.getOutputPublicValue();
-         
-        addEqualityAssertion(check2, W, "check2");
-        
+        ECGroupGeneratorGadget dec1 = new ECGroupGeneratorGadget(Gx, Gy, SK);
+        ECGroupOperationGadget dec2 = new ECGroupOperationGadget(Vsum_x, Vsum_y, SK, Gx, Gy, msgsum);
+
+        Wire[] check1 = dec1.getOutputWires();
+        Wire[] check2 = dec2.getOutputWires();
+
+        addEqualityAssertion(check1[0], Ux, "check1");
+        addEqualityAssertion(check1[1], Uy, "check1");
+        addEqualityAssertion(check2[0], Wsum_x, "check2");
+        addEqualityAssertion(check2[1], Wsum_y, "check2");
     }
+
+	SecureRandom GlobalRand = new SecureRandom();
+	public BigInteger setRandombit(BigInteger maxValue){
+		byte[] rand_bytes = new byte[(maxValue.bitLength()/8)+1];
+		BigInteger out;
+		do{
+			GlobalRand.nextBytes(rand_bytes);
+			out = new BigInteger(1,rand_bytes);
+		}while(out.bitLength() != maxValue.bitLength() || out.compareTo(maxValue) == 1);
+		return out;
+	}
 
     @Override
     public void generateSampleInput(CircuitEvaluator circuitEvaluator) {
-        circuitEvaluator.setWireValue(G, new BigInteger("10398164868948269691505217409040279103932722394566360325611713252123766059173"));
-        circuitEvaluator.setWireValue(U, new BigInteger("8770841330403347030926649719068993689202161186696114159318225920256745879147"));
-        circuitEvaluator.setWireValue(V, new BigInteger("18437695250178433367117794068340095227102603266608906145263062046682943112561"));
-        circuitEvaluator.setWireValue(W, new BigInteger("18846636015165217144662772718052395666868227456945327617274249870237629825709"));
+        int idx;
+		BigInteger Big_msg, Big_msg_sum, rand;
+        Curve25519 EC_G, EC_U;
+        Curve25519 EC_S, EC_T;
+        Curve25519 EC_V, EC_W;
 
-        circuitEvaluator.setWireValue(msgsum, new BigInteger("2147483648"));
-        
-        
-        circuitEvaluator.setWireValue(SK, new BigInteger("204444782122713504954636029222746100201332865755450300886921118968015889151"));
-        
-             
+        EC_G = new Curve25519(GroupElement.G, false);
+        EC_U = new Curve25519(GroupElement.U, false);
+        Big_msg_sum = BigInteger.ZERO;
+
+        Curve25519 EC_Vsum = null, EC_Wsum = null;
+        System.out.println("Generate " + this.numofelector + " people test cases.");
+
+        // This for loop creates a voter's message assuming the counting situation.
+        for(int i = 1; i <= this.numofelector; i++)     
+        {
+            idx = (GlobalRand.nextInt(15));							    //idx : Android seccret value
+            Big_msg = (new BigInteger("0")).setBit(16*idx);				// Big_msg : Android seccret value 
+
+            EC_S = EC_G.mul(setRandombit(GroupElement.SUBGROUP_ORDER)); // Generate for loop
+            EC_T = EC_S.mul(GroupElement.rho).add(EC_G);				
+
+            rand = setRandombit(GroupElement.SUBGROUP_ORDER);			// rand : Android seccret value 
+            EC_V = (EC_G.mul(rand)).add(EC_S.mul(Big_msg));             // EC_V : Android public voting value 
+            EC_W = (EC_U.mul(rand)).add(EC_T.mul(Big_msg));             // EC_W : Android public voting value
+
+            EC_Vsum = EC_Vsum == null ? EC_V : EC_Vsum.add(EC_V);
+            EC_Wsum = EC_Wsum == null ? EC_W : EC_Wsum.add(EC_W);
+            Big_msg_sum = Big_msg_sum.add(Big_msg);                     
+            if(i%100 == 0)
+                System.out.println(i+" people generated");
+        }
+        byte[] arrBytes = Big_msg_sum.toByteArray();
+        for(byte b : arrBytes)
+            System.out.println(Integer.toHexString(b));
+
+        System.out.println("Msg Sum:: " + Big_msg_sum.toString(16));
+        System.out.println("EC_Vsum:: " + EC_Vsum.getPoint().toString(16));
+        System.out.println("EC_Wsum:: " + EC_Wsum.getPoint().toString(16));
+
+        circuitEvaluator.setWireValue(Gx, EC_G.getPoint().x);
+        circuitEvaluator.setWireValue(Gy, EC_G.getPoint().y);
+        circuitEvaluator.setWireValue(Ux, EC_U.getPoint().x);
+        circuitEvaluator.setWireValue(Uy, EC_U.getPoint().y);
+        circuitEvaluator.setWireValue(Vsum_x, EC_Vsum.getPoint().x);
+        circuitEvaluator.setWireValue(Vsum_y, EC_Vsum.getPoint().y);
+        circuitEvaluator.setWireValue(Wsum_x, EC_Wsum.getPoint().x);
+        circuitEvaluator.setWireValue(Wsum_y, EC_Wsum.getPoint().y);
+
+        circuitEvaluator.setWireValue(msgsum, Big_msg_sum);
+        circuitEvaluator.setWireValue(SK, GroupElement.rho);
     }
 
     public static void main(String[] args) throws Exception{
-        Tally tally = new Tally("tally", 16, 15);
+        int number_of_voter;
+        if(args.length == 0 || args.length > 1)
+            number_of_voter = 1000;
+        else
+            number_of_voter = Integer.parseInt(args[0].trim());
+
+        Tally tally = new Tally("tally", number_of_voter);
         tally.generateCircuit();
         tally.evalCircuit();
         tally.prepFiles();
         tally.runLibsnark();
     }
-
 }
